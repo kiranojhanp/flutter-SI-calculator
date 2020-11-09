4ca50bc8f87a04ebc38d0174158ec80caa5f5780
Kiran ojha <kiranojhanp>
2020-11-09 18:41:08 +0545

    First screen with function

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'app_screens/first_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "My Flutter App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App Screen"),
+        ),
+        body: FirstScreen(),
+      ),
+    );
+  }
+}
