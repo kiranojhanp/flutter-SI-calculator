840ba92095b2b983d7f8fc91237b5ad0f91cd333
Kiran ojha <kiranojhanp>
2020-11-10 08:36:53 +0545

    List view

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,47 @@ import 'package:flutter/material.dart';
 import './app_screens/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Exploring UI widgets',
-    home: Home(),
-  ),);
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Exploring UI widgets',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Basic List view'),
+        ),
+        body: getListView(),
+      ),
+    ),
+  );
 }
 
+Widget getListView() {
+  var listview = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text('Landscape'),
+        subtitle: Text('Beautiful view'),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () {
+          debugPrint('Landscape tapped hehe');
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_mac),
+        title: Text('Mac'),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone_android),
+        title: Text('Phone'),
+      ),
+      Text('Yet another text element'),
+      Container(
+        color: Colors.red,
+        height: 50.0,
+      ),
+    ],
+  );
 
+  return listview;
+}
